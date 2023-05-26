675be71d0e47e19e7b9a39ba7d68de9f913b533d
unknown <wknowpark797@gmail.com>
2023-05-26 17:16:43 +0900

    탭 자바스크립트 추가

--- js/tab.js
+++ js/tab.js
@@ -6,3 +6,46 @@
   - transition 모션과 스크립트 제어 모션(animate) 따로 만들기
   - transform 제어 X
 */
+
+const frame = document.querySelector('main');
+const btns = frame.querySelectorAll('nav ul li');
+const boxs = frame.querySelectorAll('section article');
+const convertedSpeed = convertSpeed(boxs[0]);
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		if (e.currentTarget.classList.contains('on')) {
+			return;
+		}
+
+		activation(btns, idx);
+		activation(boxs, idx);
+
+		matchHeight(idx);
+	});
+});
+
+function activation(arr, idx) {
+	for (const el of arr) el.classList.remove('on');
+	arr[idx].classList.add('on');
+}
+
+function matchHeight(idx) {
+	const onHeight = parseInt(getComputedStyle(boxs[idx]).height);
+
+	new Anime(frame, {
+		prop: 'height',
+		value: onHeight,
+		duration: convertedSpeed,
+	});
+}
+
+function convertSpeed(el) {
+	return parseFloat(getComputedStyle(el).transitionDuration) * 1000;
+}
+
+/* 
+  과제: 모션 실행중 재이벤트 방지
+*/
