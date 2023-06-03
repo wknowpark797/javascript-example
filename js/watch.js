a597464a2818837ef8934636c6b86241961a1056
unknown <wknowpark797@gmail.com>
2023-06-03 09:29:48 +0900

    시계 자바스크립트 추가

--- js/watch.js
+++ js/watch.js
@@ -1,17 +1,64 @@
-const numbers = document.querySelectorAll('.screen span');
-const [am, pm] = document.querySelectorAll('.screen em');
+const main = document.querySelector('main');
+const numbers = main.querySelectorAll('.screen span');
+const [am, pm] = main.querySelectorAll('.screen em');
 
 // setInterval: 특정 시간마다 반복
 setInterval(() => {
+	changeTheme();
 	const times = getTime();
 	times.forEach((num, idx) => setTime(num, idx));
 }, 1000);
 
+function changeTheme() {
+	const hour = new Date().getHours();
+
+	// data를 전역변수로 분리할 수 있는 방법 고민하기
+	const data = [
+		{
+			cond: hour >= 5 && hour < 11,
+			name: 'morning',
+		},
+		{
+			cond: hour >= 11 && hour < 16,
+			name: 'afternoon',
+		},
+		{
+			cond: hour >= 16 && hour < 19,
+			name: 'evening',
+		},
+		{
+			cond: hour >= 19 || hour < 5,
+			name: 'night',
+		},
+	];
+
+	data.forEach((item) => {
+		if (item.cond) {
+			main.className = '';
+			main.classList.add(item.name);
+		}
+	});
+
+	if (main.classList.contains('afternoon')) {
+		main.classList.add('dark');
+	} else {
+		main.classList.remove('dark');
+	}
+
+	/*
+		[ 삼항 연산자 ]
+		표현식을 활용한 간소화된 조건문 형태
+		리액트의 JSX 내부에서 사용 가능한 제어문
+
+		조건식 ? true일 때 실행할 구문 : false일 때 실행할 구문
+	*/
+}
+
 function getTime() {
 	const now = new Date();
 	let hour = now.getHours();
-	const minute = now.getMinutes();
-	const second = now.getSeconds();
+	let minute = now.getMinutes();
+	let second = now.getSeconds();
 
 	if (hour > 12) {
 		hour -= 12; // 13(시) -> 01(시) 변환
